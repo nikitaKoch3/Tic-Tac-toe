a6f6baa9b5254e6b1a09e7aba8f97ff83045d756
Nikita Kochubey <nikitakochubey@Nikitas-iMac.local>
2024-02-15 15:05:56 -0500

    Alerts are implemented, working on AI algorythem

--- Tic-tac-toe/Tic-tac-toe/ContentView.swift
+++ Tic-tac-toe/Tic-tac-toe/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
     @State private var isHumanTurn: Bool = true
     @State private var isGameBoardDisabled: Bool = false
-    
+    @State private var alertItem: AlertItem?
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -39,19 +39,20 @@ struct ContentView: View {
                                 isGameBoardDisabled = true
                                 
                                 if checkWinCondition(for: .human, in: moves) {
-                                    print("Human wins")
+                                    alertItem = AlertContext.humanWins
                                 }
                                 
                                 if checkForDraw(in: moves) {
-                                    print("draw")
+                                    alertItem = AlertContext.draw
                                 }
                                 
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                     let position = determineComputerMovePostion(in: moves)
                                     moves[position] = Move(player: .computer, boardIndex: position)
                                     isGameBoardDisabled = false
+                                    
                                     if checkWinCondition(for: .computer, in: moves) {
-                                        print("Computer wins")
+                                        alertItem = AlertContext.computerWins
                                     }
                                 }
                             }
@@ -62,6 +63,12 @@ struct ContentView: View {
             }
             .disabled(isGameBoardDisabled)
             .padding()
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: .default(alertItem.buttonTitle, action: {
+                    print()
+                    resetGame()
+                }))
+            }
         }
     }
     
@@ -69,31 +76,30 @@ struct ContentView: View {
         return !moves.contains(where: { $0?.boardIndex == index})
     }
     func determineComputerMovePostion(in moves: [Move?]) -> Int {
-        var position = Int .random(in: 0..<9)
-        while !isSquareOccupied(in: moves, forIndex: position) {
-            position = Int.random(in: 0..<9)
+            
+            let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+            
+        
+        let computerMoves = moves.compactMap { $0 }.filter { $0.player == .computer }
+        let computerPositions = Set(computerMoves.map { $0.boardIndex })
+            
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(computerPositions)
+            
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(in: <#T##[Move?]#>, forIndex: <#T##Int#>)
+            }
         }
-        return position
-    }
+        
+            func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap{$0}.count == 9 && !checkWinCondition(for: .human, in: moves) && !checkWinCondition(for: .computer, in: moves)
+     }
     
-    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
-        
-        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-        
-        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
-        let playerPostions = Set(playerMoves.map { $0.boardIndex })
-        
-        for pattern in winPatterns where pattern.isSubset(of: playerPostions) { return true }
-        
-        
-        
-        return false
-    }
-    func checkForDraw(in moves: [Move?]) -> Bool {
-        return moves.compactMap{$0}.count == 9
+    
+    func resetGame() {
+        moves = Array(repeating: nil, count: 9)
     }
 }
-
 
 enum Player {
     case human, computer
